faa3f17cefd6d7549b2e4593c5d83816262e2638
Tanju BOZOK <tanjubozok@hotmail.com>
2025-03-09 23:53:44 +0300

    updated

--- TaskManagementApp.WebUI/Controllers/AccountController.cs
+++ TaskManagementApp.WebUI/Controllers/AccountController.cs
@@ -85,7 +85,6 @@ public class AccountController : Controller
 
     public async Task<IActionResult> LogOut()
     {
-        // Clear the existing external cookie
         await HttpContext.SignOutAsync(
             CookieAuthenticationDefaults.AuthenticationScheme);
 
@@ -106,28 +105,14 @@ public class AccountController : Controller
 
         var authProperties = new AuthenticationProperties
         {
-            //AllowRefresh = <bool>,
-            // Refreshing the authentication session should be allowed.
-
             ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30),
-            // The time at which the authentication ticket expires.
-            // A value set here overrides the ExpireTimeSpan option of CookieAuthenticationOptions set with AddCookie.
-
             IsPersistent = rememberMe,
-            // Whether the authentication session is persisted across  multiple requests.
-            // When used with cookies, controls whether the cookie's lifetime is absolute (matching the lifetime of the authentication ticket) or session-based.
-
-            //IssuedUtc = <DateTimeOffset>,
-            // The time at which the authentication ticket was issued.
-
-            //RedirectUri = <string>
-            // The full path or absolute URI to be used as an http 
-            // redirect response value.
         };
 
         await HttpContext.SignInAsync(
             CookieAuthenticationDefaults.AuthenticationScheme,
             new ClaimsPrincipal(claimsIdentity),
-            authProperties);
+            authProperties
+        );
     }
 }
